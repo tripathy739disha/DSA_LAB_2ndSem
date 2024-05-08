16ca87c47c76a034f8e0c0a10222dd1f50e1f5e6
AMARESH SWAIN <50235506+amr7503@users.noreply.github.com>
2024-05-08 12:26:35 +0000

    updated search() in LinkedlIST

--- ASSIGNMENT 04 LinkedList/LinkedList.java
+++ ASSIGNMENT 04 LinkedList/LinkedList.java
@@ -1,47 +1,56 @@
 import java.util.Scanner;
+
 class Node{
   protected int regd_no;
   protected float mark;
   protected Node next;
-  Node(int regd_no,float mark,Node next)
+  /*Node(int regd_no,float mark,Node next)
   {
     this.regd_no=regd_no;
     this.mark=mark;
     this.next=next;
-  }
+  }*/
 }
+/////////////////////////////////////////////////////////////////////////////////// 
 public class LinkedList{
   
   public static void main(String[] args){
-    Node head=new Node(0,0,null);
-    create(head);
-    create(head);
-    create(head);
-    create(head);
-    display(head);
+    Node head=null;
+    head=create(head);
+    search(head);
+    //display(head);
   }
+ /////////////////////////////////////////////////////////////////////////////////// 
+public static Node create(Node start){
   
-public static void create(Node start){
   Scanner sc=new Scanner(System.in);
+  char ch;
+  do{
+  Node node=new Node();
   System.out.println("Enter the regd_no and mark");  
-  int regd_no=sc.nextInt();
-  float mark=sc.nextFloat();
- Node new_node=new Node(regd_no,mark,null );
+  node.regd_no=sc.nextInt();
+  node.mark=sc.nextFloat();
+ //Node new_node=new Node(regd_no,mark,null );
  /*new_node.regd_no=regd_no;
  new_node.mark=mark;
  new_node.next=null; */
 
   if(start==null){
-    start=new_node;
+    start=node;
   }
  else{
   Node temp=start;
   while(temp.next!=null){
     temp=temp.next;
   }
-  temp.next=new_node;
+  temp.next=node;
  }
+    System.out.println("Would you like to enter another node(y/n)");
+    ch=sc.next().charAt(0);
+  }while(ch=='y' ||ch=='Y');
+  return start;
 }
+  /////////////////////////////////////////////////////////////////////////////////// 
 public static void display(Node start){
   Node temp=start;
   while(temp!=null){
@@ -49,24 +58,43 @@ public static void display(Node start){
     temp=temp.next;
   }
 }
-
+  /////////////////////////////////////////////////////////////////////////////////// 
+public static void search(Node start){
+  Scanner sc=new Scanner(System.in);
+  System.out.println("Enter regd_no to search");
+  int reg=sc.nextInt();
+  int c=1;
+  Node temp=start;
+  while(temp!=null){
+    if(temp.regd_no==reg){
+      System.out.println(reg+" found at node "+c);
+      return;
+    }
+    temp=temp.next;
+    c++;
+  }
+  System.out.println(reg+" not found");
+}
+  /////////////////////////////////////////////////////////////////////////////////// 
 public static Node InsBeg(Node start){
   Scanner sc=new Scanner(System.in);
   System.out.println("Enter the regd_no and mark");  
-  int regd_no=sc.nextInt();
-  float mark=sc.nextFloat();
-  Node new_node=new Node(regd_no,mark,null );
+  Node new_node=new Node();
+  new_node.regd_no=sc.nextInt();
+  new_node.mark=sc.nextFloat();
+  
  
     new_node.next=start;
     start=new_node;
   return start;
 }
+  /////////////////////////////////////////////////////////////////////////////////// 
   public static Node InsEnd(Node start){
-    Scanner sc=new Scanner(System.in)
+    Scanner sc=new Scanner(System.in);
     System.out.println("Enter the regd_no and mark");
-    int regd_no=sc.nextInt();
-    float mark=sc.nextFloat();
-    Node new_node=new Node(regd_no,mark,null );
+    Node new_node=new Node();
+    new_node.regd_no=sc.nextInt();
+    new_node.mark=sc.nextFloat();
     Node temp=start;
     while(temp.next!=null){
       temp=temp.next;
@@ -74,13 +102,14 @@ public static Node InsBeg(Node start){
     temp.next=new_node;
     return start;
   }
+  /////////////////////////////////////////////////////////////////////////////////// 
   public static Node InsAny(Node start)
   {
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter the regd_no and mark");
-    int regd_no=sc.nextInt();
-    float mark=sc.nextFloat();
-    Node new_node=new Node(regd_no,mark,null );
+    Node new_node=new Node();
+    new_node.regd_no=sc.nextInt();
+    new_node.mark=sc.nextFloat();
     Node temp=start;
     System.out.println("Enter the position to insert the node");
     int pos=sc.nextInt();
@@ -90,14 +119,16 @@ public static Node InsBeg(Node start){
     temp.next=new_node;
     return start;
   }
+  /////////////////////////////////////////////////////////////////////////////////// 
   public static Node DelBeg(Node start){
     Node temp=start;
     start=start.next;
     temp.next=null;
     return start;
   }
+  /////////////////////////////////////////////////////////////////////////////////// 
   public static Node DelEnd(Node start){
-    if(temp.next==null || temp==null) return null;
+    if(start.next==null || start==null) return null;
     Node temp=start;
     while(temp.next.next!=null){
       temp=temp.next;
@@ -105,4 +136,5 @@ public static Node InsBeg(Node start){
     temp.next=null;
     return start;
   }
+  /////////////////////////////////////////////////////////////////////////////////// 
 }
